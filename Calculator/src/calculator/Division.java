85cc93b2c973dd197dbca3e390e3d7958f82876b
„Pál <pallaszlo@uni.sapientia.ro>
2021-09-23 22:00:54 +0300

    Multipl, Division

---
+++ Calculator/src/calculator/Division.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author User
+ */
+public class Division implements Operation {
+
+    @Override
+    public double calculateResult(double left, double right) {
+        return left / right;
+    }
+}
