c3e89ed961457ab437dbccbcea76d0140bd2f75d
„Pál <pallaszlo@uni.sapientia.ro>
2021-09-23 21:55:47 +0300

    Advanced calc

---
+++ Calculator/src/calculator/AdvancedCalculator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author User
+ */
+public class AdvancedCalculator extends Calculator{
+
+    public AdvancedCalculator(double operand1, double operand2, char operation) {
+        super(operand1, operand2, operation);
+    }
+
+
+    
+}
